a4302ae95ef81eb519de0609356f2d74bc9b0c0f
Todd Whitelow <todd@whitelow.tech>
2020-11-08 17:47:23 -0700

    initial

--- getting-started/start-here/src/App.vue
+++ getting-started/start-here/src/App.vue
@@ -1,11 +1,26 @@
 <template>
+  <div class="app">
+    <h1>This is your first component in Vue</h1>
+    <h3>{{ message }}</h3>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App",
+  data: () => ({
+    message: "Powered by MDB!",
+  }),
 };
 </script>
 
 <style>
+.app {
+  margin: 0 auto;
+  font-family: sans-serif;
+  background-color: #ccf7e2;
+  padding: 10px;
+  border-radius: 5px;
+  max-width: 500px;
+}
 </style>
